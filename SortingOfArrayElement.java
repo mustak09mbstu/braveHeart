ae9fef02ec4eb8c9547ec14d6725f30c54c3012c
mustak09mbstu <37360197+mustak09mbstu@users.noreply.github.com>
2018-03-15 00:10:33 +0600

    commit

--- SortingOfArrayElement.java
+++ SortingOfArrayElement.java
@@ -3,25 +3,31 @@ import java.util.Scanner;
 
 public class SortingOfArrayElement {
     public static void main(String[] args){
+
+        /*Initializing variables*/
         int i,n,j;
         int temp=0;
+
+        /*Taking Array Size from User*/
 
         System.out.println("Enter Array size: ");
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         int arr[]=new int[n];
 
-
+        /*Taking Array Element from User*/
         System.out.println("Enter Array Elements: ");
         for (i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
 
+        /*Printing Given Array Element*/
         System.out.println("Before Sorting Array Elements are: ");
         for (i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
 
+        /*Checking Condition for Sorting*/
         for (i=0;i<n-1;i++){
             for (j=i+1;j<n;j++){
                 if (arr[i]>arr[j]){
@@ -32,6 +38,7 @@ public class SortingOfArrayElement {
             }
         }
 
+        /*Printing Array Elemet After Sorting*/
         System.out.println();
         System.out.println("After sorting Array Elements are: ");
         for (i=0;i<n;i++){
